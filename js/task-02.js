a0a3094876e2a5dd4aa6cb57fc6f2b7c8c76b55f
taaleksandra <olatarnas@gmail.com>
2022-10-31 10:55:45 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -12,5 +12,6 @@ const list = document.querySelector("#ingredients");
 ingredients.forEach(function callback(item, index) {
   const liItem = document.createElement("li");
   liItem.textContent = item;
+  liItem.classList.add("item");
   list.append(liItem);
 });
