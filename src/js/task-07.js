37459df9ff36077b87df3e9f65910226c8deddb0
taaleksandra <olatarnas@gmail.com>
2022-10-31 16:31:37 +0100

    task 7th

---
+++ src/js/task-07.js
@@ -0,0 +1,8 @@
+const fontSizeCtrl = document.querySelector("#font-size-control");
+const textSize = document.querySelector("#text");
+
+fontSizeCtrl.addEventListener("input", (event) => {
+  const inputValue = event.currentTarget.value;
+
+  textSize.style.fontSize = `${inputValue}px`;
+});
