469baba65b471b13bd5fc05c2d87f3d9f9bfc295
taaleksandra <olatarnas@gmail.com>
2022-10-31 10:50:42 +0100

    2nd task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach(function callback(item, index) {
+  const liItem = document.createElement("li");
+  liItem.textContent = item;
+  list.append(liItem);
+});
