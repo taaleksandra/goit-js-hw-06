753aa41cf59f2580b87dbdee348da6ab5c07d5bb
taaleksandra <olatarnas@gmail.com>
2022-10-31 17:00:40 +0100

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const colorNr = document.querySelector(".color");
+
+btn.addEventListener("click", (event) => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorNr.textContent = `${getRandomHexColor()}`;
+});
