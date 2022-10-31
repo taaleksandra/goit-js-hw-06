957cc92bd7b9008e75ebff8989007f59ad69b72a
taaleksandra <olatarnas@gmail.com>
2022-10-31 10:29:13 +0100

    1st task

---
+++ js/task-01.js
@@ -0,0 +1,32 @@
+const categories = document.querySelectorAll(".item");
+
+const par = document.body.firstElementChild;
+const list = par.nextElementSibling;
+
+// FIRST CATEGORY
+const fstItem = list.firstElementChild;
+const fstCat = fstItem.firstElementChild;
+const fstItemElem = fstItem.querySelectorAll("li");
+
+// SECOND CATEGORY
+const scItem = fstItem.nextElementSibling;
+const scCat = scItem.firstElementChild;
+const scItemElem = scItem.querySelectorAll("li");
+
+// LAST CATEGORY
+const lstItem = list.lastElementChild;
+const lstCat = lstItem.firstElementChild;
+const lstItemElem = lstItem.querySelectorAll("li");
+
+// RESULT
+console.log("Number of categories: ", categories.length);
+console.log("");
+console.log("Category: ", fstCat.textContent);
+console.log("Elements: ", fstItemElem.length);
+console.log("");
+console.log("Category: ", scCat.textContent);
+console.log("Elements: ", scItemElem.length);
+console.log("");
+console.log("Category: ", lstCat.textContent);
+console.log("Elements: ", lstItemElem.length);
+console.log("");
