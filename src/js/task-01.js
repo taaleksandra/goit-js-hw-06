b687060e9afd688475f1bfae49fd4ad3de7344bd
taaleksandra <olatarnas@gmail.com>
2022-11-04 21:29:14 +0100

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,32 +1,12 @@
 const categories = document.querySelectorAll(".item");
-
-const par = document.body.firstElementChild;
-const list = par.nextElementSibling;
-
-// FIRST CATEGORY
-const fstItem = list.firstElementChild;
-const fstCat = fstItem.firstElementChild;
-const fstItemElem = fstItem.querySelectorAll("li");
-
-// SECOND CATEGORY
-const scItem = fstItem.nextElementSibling;
-const scCat = scItem.firstElementChild;
-const scItemElem = scItem.querySelectorAll("li");
-
-// LAST CATEGORY
-const lstItem = list.lastElementChild;
-const lstCat = lstItem.firstElementChild;
-const lstItemElem = lstItem.querySelectorAll("li");
-
-// RESULTS
 console.log("Number of categories: ", categories.length);
 console.log("");
-console.log("Category: ", fstCat.textContent);
-console.log("Elements: ", fstItemElem.length);
-console.log("");
-console.log("Category: ", scCat.textContent);
-console.log("Elements: ", scItemElem.length);
-console.log("");
-console.log("Category: ", lstCat.textContent);
-console.log("Elements: ", lstItemElem.length);
-console.log("");
+
+categories.forEach((category) => {
+  const catName = category.querySelector("h2");
+  const catItems = category.querySelectorAll("ul > li");
+
+  console.log("Category: ", catName.textContent);
+  console.log("Elements: ", catItems.length);
+  console.log("");
+});
