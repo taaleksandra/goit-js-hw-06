5025b11e7376cd53afd87faf8f718ea2c5ed640e
taaleksandra <olatarnas@gmail.com>
2022-11-04 22:42:28 +0100

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divBoxes = document.querySelector("#boxes");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+
+input.value = 3;
+
+// CREATE
+const createBoxes = (amount) => {
+  let dim = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    divBoxes.append(box);
+
+    box.style.height = dim + "px";
+    box.style.width = dim + "px";
+    box.style.backgroundColor = `${getRandomHexColor()}`;
+    divBoxes.style.display = "flex";
+    divBoxes.style.gap = "10px";
+
+    dim += 10;
+  }
+};
+
+btnCreate.addEventListener("click", createBoxes(input.value));
+
+// DESTROY
+// function destroyBoxes() {}
