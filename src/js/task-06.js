bc7dbd750549682cf67cbbd4ceef0471518c1782
taaleksandra <olatarnas@gmail.com>
2022-11-04 21:33:04 +0100

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,11 +2,11 @@ const textInput = document.querySelector("#validation-input");
 const maxInputLength = textInput.dataset.length;
 
 textInput.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length > maxInputLength) {
-    textInput.classList.add("invalid");
-    textInput.classList.remove("valid");
-  } else {
+  if (event.currentTarget.value.length == maxInputLength) {
     textInput.classList.add("valid");
     textInput.classList.remove("invalid");
+  } else {
+    textInput.classList.add("invalid");
+    textInput.classList.remove("valid");
   }
 });
