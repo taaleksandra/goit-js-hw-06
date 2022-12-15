4891db86198757c79741584636cc72b0028ee8f2
taaleksandra <olatarnas@gmail.com>
2022-12-15 21:13:22 +0100

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -7,27 +7,40 @@ const btnCreate = document.querySelector("button[data-create]");
 const btnDestroy = document.querySelector("button[data-destroy]");
 const input = document.querySelector("input");
 
-input.value = 3;
-
 // CREATE
-const createBoxes = (amount) => {
+const createBoxes = () => {
+  divBoxes.innerHTML = "";
   let dim = 30;
+  let inputValue = input.value;
 
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement("div");
-    divBoxes.append(box);
+  if (inputValue > 100) {
+    alert("Maximum value is 100.");
+    return;
+  } else if (inputValue === "") {
+    alert("No boxes to create.");
+    return;
+  } else {
+    for (let i = 0; i < inputValue; i++) {
+      const box = document.createElement("div");
+      divBoxes.append(box);
 
-    box.style.height = dim + "px";
-    box.style.width = dim + "px";
-    box.style.backgroundColor = `${getRandomHexColor()}`;
-    divBoxes.style.display = "flex";
-    divBoxes.style.gap = "10px";
+      box.style.height = dim + "px";
+      box.style.width = dim + "px";
+      box.style.backgroundColor = `${getRandomHexColor()}`;
 
-    dim += 10;
+      dim += 10;
+    }
   }
 };
 
-btnCreate.addEventListener("click", createBoxes(input.value));
-
 // DESTROY
-// function destroyBoxes() {}
+function destroyBoxes() {
+  if (divBoxes.innerHTML === "") {
+    alert("No boxes to destroy.");
+  }
+  divBoxes.innerHTML = "";
+}
+
+// btn handler
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
